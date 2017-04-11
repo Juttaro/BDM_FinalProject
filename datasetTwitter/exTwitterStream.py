d0870519c37c2641ec5b26a844588d76e4e64cb7
Daniel Obi <danobi06@gmail.com>
2017-04-10 22:53:22 -0400

    enhanced functions + support for utf-8 encoding

--- datasetTwitter/exTwitterStream.py
+++ datasetTwitter/exTwitterStream.py
@@ -2,69 +2,33 @@
 
 import json
 import time
+import codecs
 from twython import Twython
 from twython import TwythonStreamer
 from httplib import IncompleteRead
 
 APP_KEY = "LWnaGn2ZbLwNa9SYzwbeFz5vQ"
 APP_SECRET = "ZQgTvpYzJhDRe0xoROkm2o6AqviZiHtiQIL9uFHS0wBINYN7Sw"
+OAUTH_TOKEN = "850102013174177792-pfalrKryY1o5mQ9WrQRnj1EBrYIaOYf"
+OAUTH_TOKEN_SECRET = "eg0Kfv0EgZ4e2aNtf6tney9lI12S4MlynXkcWIVfiVODE"
+
+twtToJSON = codecs.open('stream_twt.json','a', 'utf-8')
 
 class MyStreamer(TwythonStreamer):
     def on_success(self, data):
-        # try:
         if 'text' in data:
-            #print data['text'].encode('utf-8')
-            twtToJSON = open('stream_twt.json','a')
-            twtToJSON.write(json.dumps(data))
-            twtToJSON.close()
-                # return True
-        # except BaseException, e:
-            # print 'failed on', str(e)
-            # time.sleep(5)
-
-        #try:
-            # if 'text' in data:
-        #     temp = data
-        #     print data['text'].encode('utf-8')
-        #     twtToJSON = open('stream_twt.json','w')
-        #     twtToJSON.write(json.dumps(temp))
-        #     twtToJSON.close()
-        # except BaseException, e:
-        #     print 'failed'
-            # print data['text'].encode('utf-8')
-            # print data
-            # print data
-
+            json.dump(data, twtToJSON, ensure_ascii=False)
 
     def on_error(self, status_code, data):
         print status_code
 
+if __name__ == '__main__':
+        try:
+            print 'Streaming...'
+            stream = MyStreamer(APP_KEY, APP_SECRET,OAUTH_TOKEN,OAUTH_TOKEN_SECRET)
+            stream.statuses.filter(locations='-125,30,-65,50')
 
-twitter = Twython(APP_KEY, APP_SECRET)
-
-# auth = twitter.get_authentication_tokens()
-#
-# OAUTH_TOKEN = auth['oauth_token']
-# OAUTH_TOKEN_SECRET = auth['oauth_token_secret']
-#
-# print OAUTH_TOKEN,OAUTH_TOKEN_SECRET, auth['auth_url']
-
-OAUTH_TOKEN = "850102013174177792-pfalrKryY1o5mQ9WrQRnj1EBrYIaOYf"
-OAUTH_TOKEN_SECRET = "eg0Kfv0EgZ4e2aNtf6tney9lI12S4MlynXkcWIVfiVODE"
-
-while True:
-    try:
-        stream = MyStreamer(APP_KEY, APP_SECRET,OAUTH_TOKEN,OAUTH_TOKEN_SECRET)
-        stream.statuses.filter(locations='-125,30,-65,50')
-    except IncompleteRead:
-        continue
-    except BaseException, e:
-        print 'failed on', str(e)
-        time.sleep(5)
-    except KeyboardInterrupt:
-        stream.disconnect()
-        break
-
-
-# stream.statuses.filter(track='#nyc')
-# stream.statuses.filter(language='english')
+        except KeyboardInterrupt:
+            print '...Stream END'
+            twtToJSON.close()
+            stream.disconnect()
